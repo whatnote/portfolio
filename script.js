eeb58427057db122ac88bf46e73dce6d0b3678be
Daniel Culley <daniel_cornwall@hotmail.com>
2021-05-12 06:49:01 +0100

    projects added to site

---
+++ script.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+
+window.addEventListener("scroll",() => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+  navbar.classList.add("sticky");
+
+}else{
+  navbar.classList.remove("sticky");
+}
+});
